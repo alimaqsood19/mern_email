b0e49aae291a69876b6968832b67396d31ab3fbe
alimaqsood19 <ali-maqsood@Hotmail.com>
2017-08-03 02:15:38 -0400

    passport init

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const passport = require('passport'); //How to handle authentication
+const GoogleStrategy = require('passport-google-oauth20').Strategy; //Using the google oauth strategy
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
+
+//Informing passport library on which strategy we are using to authenticate
+passport.use(new GoogleStrategy()); //Creating new instance of google passport auth strategy
 
 app.listen(PORT, () => {
   console.log('Sever up on port 3000');
